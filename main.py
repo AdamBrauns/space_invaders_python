be84afe252037c90eec49eaa9499e0d889175623
Adam Brauns <braunsaj14@uww.edu>
2019-11-07 20:24:44 -0600

    added prop enemy that drops down after hitting limit

--- main.py
+++ main.py
@@ -33,25 +33,54 @@ player.speed(0)
 player.setposition(0, -250)
 player.setheading(90)
 
-playerspeed = 15
+# Create the enemy 
+enemy = turtle.Turtle()
+enemy.color("red")
+enemy.shape("circle")
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, 250)
+
+item_zone_x_y = 280
+
+enemy_speed = 2
+
+player_speed = 15
 
 # Move the player left and right
 
 def move_left():
     x = player.xcor()
-    x -= playerspeed
-    if x >= -280:
+    x -= player_speed
+    if x >= -item_zone_x_y:
         player.setx(x)
 
 def move_right():
     x = player.xcor()
-    x += playerspeed
-    if x <= 280:
+    x += player_speed
+    if x <= item_zone_x_y:
         player.setx(x)
+
+def step_down(item):
+    y = item.ycor()
+    y -= 40
+    item.sety(y)
 
 # Create keyboard bindings
 wn.listen()
 wn.onkey(move_left, "Left")
 wn.onkey(move_right, "Right")
 
-wn.mainloop()
+while True:
+    x = enemy.xcor()
+    x += enemy_speed
+    enemy.setx(x)
+    if enemy.xcor() > item_zone_x_y:
+        step_down(enemy)
+        enemy_speed *= -1
+    
+    if enemy.xcor() < -item_zone_x_y:
+        step_down(enemy)
+        enemy_speed *= -1
+
+#wn.mainloop()
