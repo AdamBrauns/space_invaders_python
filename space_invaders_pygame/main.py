2a6745157c75b3c18dc13bf7488d207ee1b6f8e9
Adam Brauns <braunsaj14@uww.edu>
2019-11-10 20:21:39 -0600

    player is now drawn on the screen

--- space_invaders_pygame/main.py
+++ space_invaders_pygame/main.py
@@ -11,9 +11,24 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("images/ufo.png")
 pygame.display.set_icon(icon)
 
+# Player image
+playerImg = pygame.image.load('images/player2.png')
+playerX = 370
+playerY = 480
+
+def player():
+    # Blit is used to draw on screen
+    screen.blit(playerImg, (playerX, playerY))
+
 # Game Loop
 running = True
 while True:
+    # RGB - Red, Green, Blue (goes up to 255)
+    screen.fill((0, 0, 0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
