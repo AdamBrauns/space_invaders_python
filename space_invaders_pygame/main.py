215df0b978a5001510cae44e752a710292519f7a
Adam Brauns <braunsaj14@uww.edu>
2019-11-10 20:37:26 -0600

    printing out the keystrokes before adding movement to player

--- space_invaders_pygame/main.py
+++ space_invaders_pygame/main.py
@@ -16,19 +16,29 @@ playerImg = pygame.image.load('images/player2.png')
 playerX = 370
 playerY = 480
 
-def player():
+def player(x, y):
     # Blit is used to draw on screen
-    screen.blit(playerImg, (playerX, playerY))
+    screen.blit(playerImg, (x, y))
 
 # Game Loop
 running = True
 while True:
     # RGB - Red, Green, Blue (goes up to 255)
     screen.fill((0, 0, 0))
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # If keystroke is pressed check if it is right or left
+        if event.type == pygame.KEYDOWN:
+            print("A keystroke has been pressed")
+            if event.key == pygame.K_LEFT:
+                print("Left arrow pressed")
+            if event.key == pygame.K_RIGHT:
+                print("Right arrow pressed")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Keystroke has been released")
+
+    player(playerX, playerY)
     pygame.display.update()
